09c4cfc619d730a4cf7964ae47aec41440cf4a56
joonghunsung <jjjr0617@gmail.com>
2026-08-19 22:05:27 +0900

    missing player commit

--- test/mock_player_repository_test.dart
+++ test/mock_player_repository_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:onetouch/data/players/mock_player_repository.dart';
+import 'package:onetouch/data/players/player_repository_provider.dart';
 import 'package:onetouch/data/teams/mock/team_catalog.dart';
 import 'package:onetouch/data/teams/mock/team_trophy_catalog.dart';
 import 'package:onetouch/models/player.dart';
